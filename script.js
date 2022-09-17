b148f2b84da8e8e7ccae5517d086026b83d0a478
Spencer Bohol <zxtm89j1@gmail.com>
2022-09-17 17:13:20 +0800

    dates

--- script.js
+++ script.js
@@ -252,6 +252,29 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+// // Create a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+
+// Working with Dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+
+console.log(Date.now());
+future.setFullYear(2040);
+console.log(future);
+/*
 console.log(2 ** 53 - 1);
 
 // Creating Big Int numbers
@@ -260,7 +283,7 @@ console.log(BigInt(5154545454556565989868383868595965353535353535));
 
 console.log();
 
-/*
+
 // 287,460,000,000
 const diameter = 287_460_000_000;
 
